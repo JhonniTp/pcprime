adc525a69e45d51bc3684f53c37734e3ae84ad50
Jhonni <jhonnitp88@gmail.com>
2025-04-29 20:23:36 -0500

    first commit

---
+++ src/main/java/com/pcprime/pcprime/PcprimeApplication.java
@@ -0,0 +1,13 @@
+package com.pcprime.pcprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcprimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcprimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcprime/pcprime/PcprimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcprime.pcprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcprimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
