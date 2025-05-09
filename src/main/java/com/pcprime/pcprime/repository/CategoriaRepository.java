311e8b393f3e3d8659ce1e5087d7d9a3d4a3f34a
jhonnitp88 <jhonnitp88@gmail.com>
2025-05-09 21:18:51 +0000

    rest system

--- src/main/java/com/pcprime/pcprime/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pcprime.pcprime.repository;
-
-import com.pcprime.pcprime.model.CategoriaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<CategoriaModel, Integer> {
-}
--- src/main/java/com/pcprime/pcprime/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pcprime.pcprime.repository;
-
-import com.pcprime.pcprime.model.UsuarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
-}
